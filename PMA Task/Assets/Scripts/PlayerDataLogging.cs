2f9441694529ebf42de79e11175fb777ffc44570
Skylar DeWitt <dewitt.skylar@gmail.com>
2026-02-16 15:41:52 -0500

    Better pre and post storm location output
    More clear in the .txt file what 0, 1, and 2 mean for the PreStormLocation and StormEndLocation.

--- PMA Task/Assets/Scripts/PlayerDataLogging.cs
+++ PMA Task/Assets/Scripts/PlayerDataLogging.cs
@@ -18,7 +18,7 @@ public class PlayerDataLogging : MonoBehaviour
         folderPath = FolderManager.Instance.SessionFolderPath;
         filePath = Path.Combine(folderPath, fileName);
         // Write header row
-        logLines.Add("Trial\tStorm\tShock\tExpectancy\tPreStormLocation\tStormEndLocation\tPoints\tMiningTime\tShelterTime\tTimeNotInShelterMining");
+        logLines.Add("Trial\tStorm\tShock\tExpectancy\tPreStormLocation(0=inbetween;1=house;2=mine)\tStormEndLocation(0=inbetween;1=house;2=mine)\tPoints\tMiningTime\tShelterTime\tTimeNotInShelterMining");
     }
 
     //Add row of trial data
